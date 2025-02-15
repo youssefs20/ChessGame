eb1240ea5cea1cba6d10d1a975fb6ee0656147ad
Snake-EyE-12 <chase.guymon@q.com>
2025-02-14 21:39:55 -0700

    TestSetup

---
+++ UnitTesting/Tests.cs
@@ -0,0 +1,22 @@
+namespace UnitTesting
+{
+
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+
+        // [Test]
+        // public void FactoryGameModeConversionTest_HappyPath()
+        // {
+        //     //Arrange
+        //     ChessLogic.GameModeFactory factory = new ChessLogic.GameModeFactory();
+        //     //Act
+        //     //Assert
+        //     ChessLogic.
+        // }
+    }
+}
