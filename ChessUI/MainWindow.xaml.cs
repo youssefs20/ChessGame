e2cfb09e4edd135adb3742e7b3a0a28147186872
youssef sameh <yousef17453@gmail.com>
2025-02-19 16:45:40 +0200

    added a pause & restart menu to game ui

--- ChessUI/MainWindow.xaml.cs
+++ ChessUI/MainWindow.xaml.cs
@@ -233,9 +233,34 @@ namespace ChessUI
 
         private void RestartGame()
         {
+            selectedPos = null;
             HideHighlights();
             moveCache.Clear();
             GetGameModeChoice();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(!IsMenuOnScreen() && e.Key == Key.Escape)
+            {
+                ShowPauseMenu();
+            }
+        }
+
+        private void ShowPauseMenu()
+        {
+            PauseMenu pauseMenu = new PauseMenu();
+            MenuContainer.Content = pauseMenu;
+
+            pauseMenu.OptionSelected += option =>
+            {
+                MenuContainer.Content = null;
+
+                if (option == Option.Restart)
+                {
+                    RestartGame();
+                }
+            };
         }
     }
 }
